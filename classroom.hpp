9884f28b432538d2fbe5179fc0e0ac393bfa62c1
Jeremiah Thomas <Bizzlebin@users.noreply.github.com>
2020-11-24 13:58:47 -0500

    = ClassRoom (.hpp; adjusted styling to match guidelines)

--- classroom.hpp
+++ classroom.hpp
@@ -26,35 +26,38 @@
 #include <string>
 
 using namespace std;
-
-
+// 
+// +++
+// Functions
+// 
 // ===
 // Classes
 // 
+// ---
+// ClassRoom
+// 
+class ClassRoom
+	{
+	public:
+		string name = "CSC134";
+		int student_count;
+		Student* students = new Student[MAX_CLASS_SIZE];
 
-class ClassRoom {
-public:
-    string name = "CSC134";
-    int student_count;
-    Student* students = new Student[MAX_CLASS_SIZE];
+		// Constructor
+		ClassRoom();
+		//ClassRoom();
 
-    // Constructor
-    ClassRoom();
-    //ClassRoom();
+		// Destructor
+		~ClassRoom();
 
-    // Destructor
-    ~ClassRoom();
 
-    // 
-    // +++
-    // Functions
-    // 
-    void create_students();
-    void update_students_order_by_average_grade();
-    void update_students_order_by_last_name();
-    void create_class_average_grade();
-    void output_class_details();
-    void read_student_count();
-private:
-    const int MAX_CLASS_SIZE = 24;
-};
+		void create_students();
+		void update_students_order_by_average_grade();
+		void update_students_order_by_last_name();
+		void create_class_average_grade();
+		void output_class_details();
+		void read_student_count();
+
+	private:
+		const int MAX_CLASS_SIZE = 24;
+	};

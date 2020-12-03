052ad441efacbfe5cd6056ed478e9573e0f32f2c
1RandomUser <shanecawthon@gmail.com>
2020-12-03 12:00:46 -0500

    added comments. Tried to keep it in a similar format to the classroom header to try and maintain uniformity.

--- student.hpp
+++ student.hpp
@@ -1,10 +1,20 @@
+///////////////////////////////////////////////////////////////////////
+//                                                                     
+// Filename: student.hpp
+// Date: November 24, 2020
+// Programmer: Shane Cawthon  //  William Lineberger    
+//
+// Description:
+//    Header file for Student class.
+/////////////////////////////////////////////////////////////////////////
+
 // Student
 // 
 // https://github.com/bizzlebin/team_c/blob/master/student.hpp
 // 
 // ***
 // 
-// By Jeremiah Thomas, et al
+// By Shane Cawthon, William Lineberger et al
 // 
 // ***
 // 
@@ -29,6 +39,41 @@
 #include <iomanip>
 
 using namespace std;
+
+///////////////////////////////////////////////////////////////////////
+//
+// Class: Student                                      
+//                                                                   
+// Description:
+//    Student objects get and set the attributes for each student, 
+//    return the average exam grade, and display the attributes for each student
+//
+//    List of data members:
+//    string     FIRST_NAME
+//    string     LAST_NAME
+//    string     ssn
+//    double     grades[4]
+//    double     average_grade
+//    static int student_count
+//
+//    List of member functions:
+//    Student()
+//    Student(string FIRST_NAME, string LAST_NAME, string ssn, double grades[4])
+//    ~Student()
+//    string read_first_name() const
+//    string read_last_name() const
+//    void update_ssn(string ssn)
+//    string read_ssn()
+//    void update_grades(double grades[4])
+//    const double* read_grades() const
+//    void update_average_grade(double average_grade)
+//    double read_average_grade()
+//    static int read_student_count()
+//    double create_average_grade()
+//    void output_details()
+//    
+///////////////////////////////////////////////////////////////////////
+
 // 
 // +++
 // Functions
@@ -36,6 +81,9 @@ using namespace std;
 // ===
 // Classes
 // 
+// ---
+// Student
+//
 class Student
 	{
 	public: 
